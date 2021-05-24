1dd71289be29e96dc4b60cd6c2add01960d5c129
Handsome Jack <jack@hyperion>
2021-05-25 03:18:35 +0530

    added parent.html, new_video.html and modified index.html, views.py and urls.py

--- youtube/views.py
+++ youtube/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.views.generic.base import  View,HttpResponse
 
+
 # Create your views here.
 class Index(View):
     template_name='index.html'
     def get(self,request):
-        variableA ='some text here'
+        variableA ='title'
         return render(request,self.template_name,{'variableA':variableA})
     def post(self,request):
+        return HttpResponse('this is index view. post request')
+
+
+class NewVideo(View):
+    template_name = 'new_video.html'
+
+    def get(self, request):
+        variableA = 'new video'
+        return render(request, self.template_name, {'variableA': variableA})
+
+    def post(self, request):
         return HttpResponse('this is index view. post request')
--- youtube_python/urls.py
+++ youtube_python/urls.py
@@ -1,9 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path
-from youtube.views import Index
+from youtube.views import Index, NewVideo
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index', Index.as_view())
+    path('index/', Index.as_view()),
+    path('new_video/', NewVideo.as_view())
+
 ]
